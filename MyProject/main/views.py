d933838e8d8a53b0d25cf58ca2d8972680958703
Stoyan <stoian.makendzhiev@gmail.com>
2024-02-16 15:02:47 +0200

    index.html done

---
+++ MyProject/main/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def main(request):
+    return render(request, 'main/main.html')
+
+def single(request):
+    return render(request, 'main/single.html')
+
